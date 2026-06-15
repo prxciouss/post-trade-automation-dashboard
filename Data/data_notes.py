0befc0da41fd04670b6f2e167611d14861f70203
Precious Adagunodo <your-email@example.com>
2026-06-15 02:47:29 +0100

    Clean up project structure

--- Data/data_notes.py
+++
@@ -1,5 +0,0 @@
-# A csv is a Comma-Separated Values. It’s basically a very simple spreadsheet file.
-    #eg. name, age, job
-    #precious, 21, unemployed
-
-# CSV's are lightweight, simple, easy for programs to read
